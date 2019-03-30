b88e0a8e99410148a5585985fc4e555c3b30212f
Robert Rand <rxtreme@gmail.com>
2019-03-30 00:19:59 -0400

    early NDSem work

--- NDSem.v
+++ NDSem.v
@@ -17,8 +17,6 @@ Search (bool -> bool -> bool).
 Definition count (len : nat) (f : list bool -> bool) (b : bool): nat :=
   length (filter (fun l => (bool_eq (f l) b)) (enumerate_bool_lists len)).
 
-Search count.
-
 (* /MOVE *)
 
 (* But also move *)
@@ -26,25 +24,28 @@ Search count.
 Definition norm {n} (ψ : Vector n) :=
   sqrt (fst (ψ ∘ ψ†)).  
 
-Inductive nd_eval : forall dim, com -> Vector (2^dim) -> Vector (2^dim) -> Prop :=
-  | nd_app : forall dim n (u : Unitary n) (l : list nat) (ψ : Vector (2^dim)),
-      nd_eval dim (app u l) ψ ((ueval dim u l) × ψ)
-  | nd_meas0 : forall dim n (ψ : Vector (2^dim)),
+(* With scaling *)
+Reserved Notation "c '/' ψ '⇩' ψ'"
+                  (at level 40, ψ at level 39).
+
+Inductive nd_eval {dim : nat} : com -> Vector (2^dim) -> Vector (2^dim) -> Prop :=
+  | nd_skip : forall ψ, nd_eval skip ψ ψ
+  | nd_app : forall n (u : Unitary n) (l : list nat) (ψ : Vector (2^dim)),
+      (app u l) / ψ ⇩ ((ueval dim u l) × ψ)
+  | nd_meas0 : forall n (ψ : Vector (2^dim)),
       let ψ' := @pad 2 n dim (∣0⟩⟨0∣) × ψ in 
-      nd_eval dim (meas n) ψ (scale (/(norm ψ')) ψ') 
-  | nd_meas1 : forall dim n (ψ : Vector (2^dim)),
+      norm ψ' <> 0%R ->
+      (meas n) / ψ ⇩ (scale (/(norm ψ')) ψ') 
+  | nd_meas1 : forall n (ψ : Vector (2^dim)),
       let ψ' := @pad 2 n dim (∣1⟩⟨1∣) × ψ in
       norm ψ' <> 0%R ->
-      nd_eval dim (meas n) ψ (scale (/(norm ψ')) ψ') 
-.
+      (meas n) / ψ ⇩ (scale (/(norm ψ')) ψ') 
+  | nd_reset0 : forall n (ψ : Vector (2^dim)),
+      let ψ' := @pad 2 n dim (∣0⟩⟨0∣) × ψ in 
+      (meas n) / ψ ⇩ (scale (/(norm ψ')) ψ') 
+  | nd_reset1 : forall n (ψ : Vector (2^dim)),
+      let ψ' := @pad 2 n dim (∣0⟩⟨1∣) × ψ in (* is this right? *)
+      norm ψ' <> 0%R ->
+      (meas n) / ψ ⇩ (scale (/(norm ψ')) ψ')
 
-
-
-Reserved Notation "dim '$' c '/' ψ '||' ψ'" (at level 80).
-
-Inductive nd_eval : forall dim, com -> Vector (2^dim) -> Vector (2^dim) -> Prop :=
-  | nd_app : forall dim n (u : Unitary n) (l : list nat) (ψ : Vector (2^dim)),
-    (dim $ app u l / ψ || ((ueval dim u l) × ψ))
-
-where "dim '$' c '/' ψ '||' ψ'" := (nd_eval dim c ψ ψ').
-                                                                              
+where "c '/' ψ '⇩' ψ'" := (nd_eval c ψ ψ').              
