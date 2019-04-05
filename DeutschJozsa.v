4e72f1173d6bb9506ecd36881eab1800a72ccf25
Shih-Han Hung <shung@umd.edu>
2019-04-05 02:10:39 -0400

    fix deutsch constant

--- DeutschJozsa.v
+++ DeutschJozsa.v
@@ -44,24 +44,86 @@ Fixpoint nket (n : nat) (ψ : Matrix 2 1) : Matrix (2^n) 1 :=
 Lemma aux : forall a b : nat, (a + b = 0 <-> a = 0 /\ b = 0)%nat.
 Proof. intros. lia. Qed.
 
+Notation "∣+⟩" := (/√2 .* ∣0⟩ .+ /√2 .* ∣1⟩)%C.
+Notation "∣-⟩" := (/√2 .* ∣0⟩ .+ (-/√2) .* ∣1⟩)%C.
+ 
 Lemma aux2 : forall {m n : nat}(ψ1 ψ2 : Matrix m n), 
-    ψ1 ⊗ ∣0⟩ = ψ2 ⊗ ∣0⟩ -> ψ1 = ψ2.
+    ψ1 ⊗ ∣+⟩ = ψ2 ⊗ ∣+⟩ -> ψ1 = ψ2.
+Proof. Admitted.
+
+Lemma aux3 : forall {m n : nat}(ψ1 ψ2 : Matrix m n), 
+    ψ1 ⊗ ∣-⟩ = ψ2 ⊗ ∣-⟩ -> ψ1 = ψ2.
 Proof. Admitted.
 
 Lemma WT_boolean : forall (dim : nat) (U : ucom), boolean dim U -> uc_well_typed dim U.
 Proof. intros. inversion H; assumption. Qed.
- 
+
+Lemma cpar_correct :
+  forall (dim : nat), (uc_eval (S dim) (cpar1 (S dim) H)) × (∣1⟩ ⊗ (nket dim ∣0⟩)) = ∣-⟩ ⊗ (nket dim ∣+⟩).
+Proof.
+  remember (∣+⟩) as plus.
+  remember (∣-⟩) as minus.
+  induction dim as [| dim'].
+  - unfold cpar1. simpl. unfold ueval1, pad. simpl. 
+    rewrite Heqminus. solve_matrix.
+  - replace (cpar1 (S (S dim')) H) with (cpar1 (S dim') H ; H (S dim')) by (simpl; reflexivity).
+    remember (cpar1 (S dim') H) as c. simpl.
+    unfold ueval1, pad. replace (S dim' + 1)%nat with (S (S dim')) by lia. 
+    bdestructΩ (S (S dim') <=? S (S dim')).
+    rewrite Nat.sub_diag. 
+    rewrite kron_1_r.
+    replace (S (S dim'))%nat with (S dim' + 1)%nat by lia.
+    rewrite <- pad_dims.
+    remember (uc_eval (S dim') c) as u. 
+    replace (2 ^ dim' + (2 ^ dim' + 0))%nat with (2 ^ S dim') by unify_pows_two.
+    replace (2 ^ 1)%nat with 2%nat by (simpl; reflexivity).
+    replace (2 ^ S dim' + (2 ^ S dim' + 0))%nat with (2 ^ S dim' * 2)%nat by unify_pows_two.
+    rewrite kron_mixed_product.
+    rewrite Mmult_1_r by auto with wf_db. 
+    rewrite Mmult_1_l.
+    remember (nket dim' ∣0⟩) as k0.
+    remember (nket dim' plus) as kp.
+    show_dimensions.
+    replace 
+      (kron' 2 1 (2 ^ S dim') 1 ∣1⟩ (kron' (2 ^ dim') 1 2 1 k0 ∣0⟩))
+    with
+      (kron' 2 1 (2 ^ dim' * 2) (1 * 1) ∣1⟩ (kron' (2 ^ dim') 1 2 1 k0 ∣0⟩)) by (unify_pows_two; rewrite plus_comm; reflexivity).
+    replace 
+      (kron' 2 1 (2 ^ S dim') 1 minus (kron' (2 ^ dim') 1 2 1 kp plus))
+    with
+      (kron' 2 1 (2 ^ dim' * 2) (1 * 1) minus (kron' (2 ^ dim') 1 2 1 kp plus)) by (unify_pows_two; rewrite plus_comm; reflexivity).
+    repeat rewrite <- kron_assoc.
+    hide_dimensions. show_dimensions.
+    replace
+      (Mmult' (2 ^ S dim' * 2) (2 ^ S dim' * 2) 1
+              (kron' (2 ^ S dim') (2 ^ S dim') 2 2 u hadamard)
+              (kron' (2 * 2 ^ dim') (1 * 1) 2 1 (kron' 2 1 (2 ^ dim') 1 ∣1⟩ k0) ∣0⟩)) 
+    with
+      (Mmult' (2 ^ S dim' * 2) (2 ^ S dim' * 2) (1 * 1 * 1)
+              (kron' (2 ^ S dim') (2 ^ S dim') 2 2 u hadamard)
+              (kron' (2 * 2 ^ dim') (1 * 1) 2 1 (kron' 2 1 (2 ^ dim') 1 ∣1⟩ k0) ∣0⟩)) by (simpl; reflexivity).
+    hide_dimensions.
+    rewrite kron_mixed_product.
+    replace (2 * 2 ^ dim') with (2 ^ S dim') by unify_pows_two.
+    rewrite IHdim'.
+    rewrite Heqplus.
+    replace (hadamard × ∣0⟩) with ∣+⟩ by solve_matrix. reflexivity.
+Admitted.      
+
 Lemma deutsch_jozsa_constant0 : 
   forall (dim : nat) (U : ucom) (P : boolean dim U),
   (count P = 0)%nat -> 
-  ((uc_eval (S dim) U) × (∣1⟩ ⊗ (nket dim ∣0⟩))) = (∣1⟩ ⊗ (nket dim ∣0⟩)).
+  ((uc_eval (S dim) U) × (∣-⟩ ⊗ (nket dim ∣+⟩))) = (∣-⟩ ⊗ (nket dim ∣+⟩)).
 Proof.
   intros. 
   induction P; intros.
   - rewrite u1.
     simpl; Msimpl; reflexivity.
   - simpl in H. lia.
-  - intros. inversion H.
+  - 
+    remember (/√2 .* ∣0⟩ .+ /√2 .* ∣1⟩)%C as plus.
+    remember (/√2 .* ∣0⟩ .+ (-/√2) .* ∣1⟩)%C as minus.
+    intros. inversion H.
     apply aux in H1. inversion H1. 
     rewrite H0. rewrite H2.
     apply IHP1 in H0. 
@@ -77,44 +139,47 @@ Proof.
     destruct dim.
     + inversion P1.
     + clear P1. clear P2.
-      replace (nket (S dim) ∣0⟩) with (nket dim ∣0⟩ ⊗ ∣0⟩) in * by reflexivity.
+      replace (nket (S dim) plus) with (nket dim plus ⊗ plus) in * by reflexivity.
       show_dimensions.
       replace (2 ^ (S dim))%nat with (2 ^ dim * 2)%nat in * by unify_pows_two. 
       replace (2 ^ 1)%nat with 2%nat in * by (simpl; reflexivity).
-      replace (kron' 2 1 (2 ^ dim * 2) 1 ∣1⟩ (kron' (2 ^ dim) 1 2 1 (nket dim ∣0⟩) ∣0⟩)) with (kron' 2 1 (2 ^ dim * 2) (1 * 1) ∣1⟩ (kron' (2 ^ dim) 1 2 1 (nket dim ∣0⟩) ∣0⟩)) in * by reflexivity.
+      replace (kron' 2 1 (2 ^ dim * 2) 1 minus (kron' (2 ^ dim) 1 2 1 (nket dim plus) plus)) with (kron' 2 1 (2 ^ dim * 2) (1 * 1) minus (kron' (2 ^ dim) 1 2 1 (nket dim plus) plus)) in * by reflexivity.
       rewrite <- kron_assoc in *.
-      hide_dimensions. show_dimensions.
+      hide_dimensions. 
+      show_dimensions.
       replace (2 ^ (S dim + 1))%nat with (2 * 2 ^ dim * 2)%nat in * by unify_pows_two.
       replace
-        (Mmult' (2 * 2 ^ dim * 2) (2 * 2 ^ dim * 2) 1
+        (Mmult' (2 * 2 ^ dim * 2) (2 * 2 ^ dim * 2) (1 * 1)
          (kron' (2 ^ dim * 2) (2 ^ dim * 2) 2 2 (uc_eval (S dim) u1) (I 2))
-         (kron' (2 * 2 ^ dim) (1 * 1) 2 1 (kron' 2 1 (2 ^ dim) 1 ∣1⟩ (nket dim ∣0⟩))
-            ∣0⟩))
+         (kron' (2 * 2 ^ dim) (1 * 1) 2 1 (kron' 2 1 (2 ^ dim) 1 minus (nket dim plus))
+            plus))
       with
         (Mmult' (2 * 2 ^ dim * 2) (2 * 2 ^ dim * 2) (1 * 1 * 1)
          (kron' (2 ^ dim * 2) (2 ^ dim * 2) 2 2 (uc_eval (S dim) u1) (I 2))
-         (kron' (2 * 2 ^ dim) (1 * 1) 2 1 (kron' 2 1 (2 ^ dim) 1 ∣1⟩ (nket dim ∣0⟩))
-            ∣0⟩)) in * by (simpl; reflexivity).
+         (kron' (2 * 2 ^ dim) (1 * 1) 2 1 (kron' 2 1 (2 ^ dim) 1 minus (nket dim plus))
+            plus)) in * by (simpl; reflexivity).
       replace 
-        (Mmult' (2 * 2 ^ dim * 2) (2 * 2 ^ dim * 2) 1
+        (Mmult' (2 * 2 ^ dim * 2) (2 * 2 ^ dim * 2) (1 * 1)
          (kron' (2 ^ dim * 2) (2 ^ dim * 2) 2 2 (uc_eval (S dim) u2) (I 2))
-         (kron' (2 * 2 ^ dim) (1 * 1) 2 1 (kron' 2 1 (2 ^ dim) 1 ∣1⟩ (nket dim ∣0⟩))
-            ∣0⟩))
+         (kron' (2 * 2 ^ dim) (1 * 1) 2 1 (kron' 2 1 (2 ^ dim) 1 minus (nket dim plus))
+            plus))
       with
         (Mmult' (2 * 2 ^ dim * 2) (2 * 2 ^ dim * 2) (1 * 1 * 1)
          (kron' (2 ^ dim * 2) (2 ^ dim * 2) 2 2 (uc_eval (S dim) u2) (I 2))
-         (kron' (2 * 2 ^ dim) (1 * 1) 2 1 (kron' 2 1 (2 ^ dim) 1 ∣1⟩ (nket dim ∣0⟩))
-            ∣0⟩)) in * by (simpl; reflexivity).
+         (kron' (2 * 2 ^ dim) (1 * 1) 2 1 (kron' 2 1 (2 ^ dim) 1 minus (nket dim plus))
+            plus)) in * by (simpl; reflexivity).
       hide_dimensions.
       replace (2 ^ S dim)%nat with (2 * 2 ^ dim)%nat in * by (simpl; reflexivity).
       replace (2 * 2 ^ dim)%nat with (2 ^ dim * 2)%nat in * by omega.
       rewrite kron_mixed_product in *.
-      rewrite Mmult_1_l in * by auto with wf_db.
-      apply aux2 in H0. apply aux2 in H2.
+      rewrite Mmult_1_l in H0. 2 : rewrite Heqplus; auto with wf_db.
+      rewrite Mmult_1_l in H2. 2 : rewrite Heqplus; auto with wf_db.
+      rewrite Heqplus in H0. apply aux2 in H0. rewrite <- Heqplus in H0.
+      rewrite Heqplus in H2. apply aux2 in H2. rewrite <- Heqplus in H2.
       replace (S (0 + 0))%nat with 1%nat by (simpl; reflexivity).
-      replace (∣1⟩ ⊗ nket (S (S dim)) ∣0⟩) with (∣1⟩ ⊗ nket (S dim) ∣0⟩ ⊗ ∣0⟩).
+      replace (minus ⊗ nket (S (S dim)) plus) with (minus ⊗ nket (S dim) plus ⊗ plus).
       2 : { 
-        replace (nket (S (S dim)) ∣0⟩) with (nket (S dim) ∣0⟩ ⊗ ∣0⟩) by reflexivity.
+        replace (nket (S (S dim)) plus) with (nket (S dim) plus ⊗ plus) by reflexivity.
         rewrite kron_assoc. simpl. 
         show_dimensions.
         repeat rewrite <- plus_n_O.
@@ -142,36 +207,36 @@ Proof.
              (kron' (d * 2) (d * 2) 2 2 (kron' d d 2 2 M1 q0 .+ kron' d d 2 2 M2 q1) (I 2))) by (simpl; reflexivity).
       hide_dimensions.
       rewrite kron_mixed_product.
-      rewrite Mmult_1_l by auto with wf_db.
+      rewrite Mmult_1_l. 2 : rewrite Heqplus; auto with wf_db.
       apply f_equal2; try reflexivity.
-      rewrite Heqd, HeqM1, HeqM2, Heqψ, Heqq0, Heqq1 in *.
-      replace (nket (S dim) ∣0⟩) with (nket dim ∣0⟩ ⊗ ∣0⟩) by reflexivity.
+      rewrite Heqd, HeqM1, HeqM2, Heqq0, Heqq1 in *.
+      (* rewrite Heqd, HeqM1, HeqM2, Heqψ, Heqq0, Heqq1 in *. *)
+      replace (nket (S dim) plus) with (nket dim plus ⊗ plus) by reflexivity.
       show_dimensions.
       replace (2 ^ S dim)%nat with (2 ^ dim * 2)%nat by unify_pows_two.
       clear Heqd. clear HeqM1. clear HeqM2. clear Heqψ. clear Heqq0. clear Heqq1.
       clear q0. clear q1. clear ψ. clear M2. clear M1.
       replace 
-        (kron' 2 1 (2 ^ dim * 2) 1 ∣1⟩ (kron' (2 ^ dim) 1 2 1 (nket dim ∣0⟩) ∣0⟩))
+        (kron' 2 1 (2 ^ dim * 2) 1 minus (kron' (2 ^ dim) 1 2 1 (nket dim plus) plus))
       with
-        (kron' 2 1 (2 ^ dim * 2) (1 * 1) ∣1⟩ (kron' (2 ^ dim) 1 2 1 (nket dim ∣0⟩) ∣0⟩)) by (simpl; reflexivity).
+        (kron' 2 1 (2 ^ dim * 2) (1 * 1) minus (kron' (2 ^ dim) 1 2 1 (nket dim plus) plus)) by (simpl; reflexivity).
       hide_dimensions.
       rewrite <- kron_assoc.
       rewrite Mmult_plus_distr_r.
       replace (2 ^ dim * 2)%nat with (2 * 2 ^ dim)%nat by unify_pows_two.
       repeat rewrite kron_mixed_product.
-      replace (∣0⟩⟨0∣ × ∣0⟩) with (∣0⟩) by solve_matrix.
-      replace (∣1⟩⟨1∣ × ∣0⟩) with (@Zero 2 1)%nat by solve_matrix.
-      replace (2 ^ dim * 2)%nat with (2 * 2 ^ dim)%nat in H0 by unify_pows_two.
-      rewrite H0.
-      rewrite kron_0_r.
-      rewrite Mplus_0_r. reflexivity.
+      replace (∣0⟩⟨0∣ × plus) with (/√2 .* ∣0⟩) by (rewrite Heqplus; solve_matrix).
+      replace (∣1⟩⟨1∣ × plus) with (/√2 .* ∣1⟩)%nat by (rewrite Heqplus; solve_matrix).
+      replace (2 ^ dim * 2)%nat with (2 * 2 ^ dim)%nat in * by unify_pows_two.
+      rewrite H0. rewrite H2. 
+      rewrite <- kron_plus_distr_l. 
+      rewrite Heqplus. reflexivity.
 Qed.
-
 
 Lemma deutsch_jozsa_constant1 : 
   forall (dim : nat) (U : ucom) (P : boolean dim U),
   (count P = 2 ^ dim)%nat -> 
-  ((uc_eval (S dim) U) × (∣1⟩ ⊗ (nket dim ∣0⟩))) = (-1)%R .* (∣1⟩ ⊗ (nket dim ∣0⟩)).
+  ((uc_eval (S dim) U) × (∣-⟩ ⊗ (nket dim ∣+⟩))) = (-1)%R .* (∣-⟩ ⊗ (nket dim ∣+⟩)).
 Proof.
 Admitted.
 
@@ -183,7 +248,7 @@ Notation "ψ ∝ ϕ" := (proportional ψ ϕ) (at level 20).
 Theorem deutsch_jozsa_constant_correct :
   forall (dim : nat) (U : ucom) (P : boolean dim U),
   constant dim P -> 
-  ((uc_eval (S dim) U) × (∣1⟩ ⊗ (nket dim ∣0⟩))) ∝ (∣1⟩ ⊗ (nket dim ∣0⟩)).
+  ((uc_eval (S dim) U) × (∣-⟩ ⊗ (nket dim ∣+⟩))) ∝ (∣-⟩ ⊗ (nket dim ∣+⟩)).
 Proof.
   intros. inversion H.
   - exists 0%R. rewrite eulers0. rewrite Mscale_1_l.
@@ -197,11 +262,10 @@ Definition perpendicular {n : nat} (ψ ϕ : Vector n) :=
 
 Notation "ψ ⟂ ϕ" := (perpendicular ψ ϕ) (at level 20). 
 
-
 Theorem deutsch_jozsa_balanced_correct : 
   forall (dim : nat) (U : ucom) (P : boolean dim U) (ψ : Vector 2),
   balanced dim P -> 
-  ((uc_eval (S dim) U) × (∣1⟩ ⊗ (nket dim ∣0⟩))) ⟂ (ψ ⊗ (nket dim ∣0⟩)).
+  ((uc_eval (S dim) U) × (∣-⟩ ⊗ (nket dim ∣+⟩))) ⟂ (ψ ⊗ (nket dim ∣+⟩)).
 Proof.
 Admitted.
 
