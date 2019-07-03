33ae9a9bccbb5da9ef57702183ff5bd5a0560a7a
Robert Rand <rxtreme@gmail.com>
2019-07-03 13:07:12 -0400

    Proofs that Z rotation then measure = measure

---
+++ compiler/MeasureOptimize.v
@@ -0,0 +1,88 @@
+Require Import SQIRE.
+Require Import DensitySem.
+
+Local Open Scope com.
+
+Definition R_ {dim} θ n : ucom dim := uapp1 (U_R θ) n.  
+
+Lemma Cexp_mul_neg_l : forall θ, Cexp (- θ) * Cexp θ = 1.
+Proof.  
+  unfold Cexp. intros R.
+  eapply c_proj_eq; simpl.
+  - rewrite cos_neg, sin_neg.
+    field_simplify_eq.
+    repeat rewrite <- Rsqr_pow2.
+    rewrite Rplus_comm.
+    apply sin2_cos2.
+  - rewrite cos_neg, sin_neg. field.
+Qed.
+
+Lemma Cexp_mul_neg_r : forall θ, Cexp θ * Cexp (-θ) = 1.
+Proof. intros. rewrite Cmult_comm. apply Cexp_mul_neg_l. Qed.
+
+Lemma R_measure : forall dim θ n, @R_ dim θ n ; measure n ≡ measure n.
+Proof.
+  intros.
+  simpl.
+  unfold c_equiv.
+  simpl.
+  unfold compose_super, Splus, super.
+  unfold ueval1.
+  apply functional_extensionality. intros ρ.
+  unfold pad.
+  bdestruct (n + 1 <=? dim).
+  2: remove_zero_gates; reflexivity.
+  remember (dim - 1 - n)%nat as m.
+  assert (dim = n + 1 + m)%nat as E by lia.
+  rewrite E in *.
+  subst.
+  clear.
+  replace (2 ^ (n + 1 + m))%nat with (2^n * 2 * 2^m)%nat by unify_pows_two.
+  restore_dims.
+  Msimpl.
+  repeat rewrite Mmult_assoc.
+  repeat rewrite kron_mixed_product.  
+  repeat (restore_dims; rewrite <- Mmult_assoc).
+  repeat rewrite kron_mixed_product.  
+  replace (phase_shift (- θ) × ∣0⟩) with ∣0⟩ by solve_matrix.
+  replace (phase_shift (- θ) × ∣1⟩) with (Cexp (- θ) .* ∣1⟩) by solve_matrix.
+  repeat (restore_dims; rewrite Mmult_assoc).
+  replace ((⟨0∣ × phase_shift θ)) with ⟨0∣ by solve_matrix.
+  replace (⟨1∣ × phase_shift θ) with (Cexp θ .* ⟨1∣) by solve_matrix.
+  Msimpl.
+  apply f_equal2; trivial.                                                              
+  repeat (try rewrite Mscale_mult_dist_r;
+          try rewrite Mscale_mult_dist_l;
+          try rewrite Mscale_kron_dist_l;
+          try rewrite Mscale_kron_dist_r).
+  restore_dims_strong.
+  rewrite Mscale_mult_dist_l.
+  rewrite Mscale_assoc.
+  rewrite Cexp_mul_neg_l.
+  rewrite Mscale_1_l.
+  reflexivity.
+Qed.
+
+Lemma phase_pi : phase_shift PI = σz.
+Proof.
+  unfold phase_shift, σz.
+  rewrite eulers_identity.
+  replace (RtoC (-1)) with (Copp (RtoC 1)) by lca.
+  reflexivity.
+Qed.
+
+Lemma ueval_R_pi : forall dim n, ueval1 dim n (U_R PI) = ueval1 dim n U_Z. 
+Proof.
+  intros.
+  unfold ueval1; simpl.
+  rewrite phase_pi.
+  reflexivity.
+Qed.
+
+Lemma Z_measure : forall dim n, @SQIRE.Z dim n ; measure n ≡ measure n.  
+Proof.
+  intros. unfold c_equiv. simpl.
+  rewrite <- ueval_R_pi.
+  apply R_measure.
+Qed.  
+  
