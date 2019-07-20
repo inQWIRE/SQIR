b121ed842cff25f3052fefad0a4aed578ce89cff
Kartik Singhal <ks@cs.uchicago.edu>
2019-07-20 16:41:26 -0400

    Remove unnecessary use of ppx_deriving

--- QASM-to-SQIR/src/OQAST.ml
+++ QASM-to-SQIR/src/OQAST.ml
@@ -3,9 +3,8 @@ type exp =
   | Nninteger of int
   | Pi
   | Id of string
-[@@deriving show]
 
-type argument = string * int option [@@deriving show]
+type argument = string * int option
 
 type uop  =
   | CX of argument * argument
@@ -17,32 +16,25 @@ type uop  =
   | Y of argument
   | Z of argument
   | Gate of string * exp list * argument list
-[@@deriving show]
 
 type qop  =
   | Uop of uop
   | Meas of argument * argument
   | Reset of argument
-[@@deriving show]
 
 type gop  =
   | Uop of uop
-[@@deriving show]
 
 type gatedecl = string * string list option * string list
-[@@deriving show]
 
 type decl =
   | Qreg of string * int
   | Creg of string * int
-[@@deriving show]
 
 type statement  =
   | Decl of decl
   | GateDecl of gatedecl * gop list
   | Qop of qop
   | If of string * int * qop
-[@@deriving show]
 
 type program = statement list
-[@@deriving show]
