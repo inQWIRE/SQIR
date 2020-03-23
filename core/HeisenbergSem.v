9d372d224f69e847a238f62b49a67b51fe3c4c56
Robert Rand <rxtreme@gmail.com>
2020-03-23 13:46:31 -0400

    typo fix

--- core/HeisenbergSem.v
+++ core/HeisenbergSem.v
@@ -417,7 +417,7 @@ Definition bell00 : clifford_ucom 4 :=
   CNOT 2 3.
 
 Definition encode : clifford_ucom 4 :=
-    CZ 0 2; CNOT 1 3.
+    CZ 0 2; CNOT 1 2.
 
 Definition decode : clifford_ucom 4 := 
   CNOT 2 3;
@@ -425,12 +425,12 @@ Definition decode : clifford_ucom 4 :=
 
 Definition superdense := bell00 ; encode; decode.
 
-Compute (h_eval superdense [ZZ,ZZ,ZZ,ZZ]).
+Compute (h_eval superdense [ZZ,ZZ,ZZ,ZZ]). (* I, I, Z, Z *)
 Compute (h_eval superdense [II,ZZ,ZZ,ZZ]). (* Z, I, Z, Z *)
 Compute (h_eval superdense [ZZ,II,ZZ,ZZ]). (* I, Z, Z, Z *)
-Compute (h_eval superdense [II,II,ZZ,ZZ]).
-Compute (h_eval superdense [ZZ,ZZ,ZZ,II]).
-Compute (h_eval superdense [ZZ,ZZ,II,ZZ]).
+Compute (h_eval superdense [II,II,ZZ,ZZ]). (* Z, Z, Z, Z *)
+Compute (h_eval superdense [ZZ,ZZ,ZZ,II]). (* I, Z, Z, I *)
+Compute (h_eval superdense [ZZ,ZZ,II,ZZ]). (* Z, I, I, Z *)
 
 Compute (h_eval superdense [ZZ,II,II,II]). (* Z, I, I, I *)
 Compute (h_eval superdense [II,ZZ,II,II]). (* I, Z, I, I *)
@@ -440,7 +440,53 @@ Compute (h_eval superdense [II,II,II,ZZ]). (* I, Z, I, Z *)
 
 Lemma superdense_ZZ : h_eval superdense [ZZ,ZZ,ZZ,ZZ] = [II,II,ZZ,ZZ].
 Proof. reflexivity. Qed.
-  
+
+(* GHZ state *)
+
+Compute (h_eval (CNOT 0 1) [XX*ZZ, ZZ]). (* X, XZ *)
+
+Definition GHZ3 : clifford_ucom 3 :=
+  H 0;
+  CNOT 0 1;
+  CNOT 1 2.  
+
+Compute (h_eval GHZ3 [ZZ,ZZ,ZZ]). (* XZ, X, XZ *)
+
+Compute (h_eval GHZ3 [XX,II,II]). (* Z, I, I *)
+Compute (h_eval GHZ3 [II,XX,II]). (* I, X, X *)
+Compute (h_eval GHZ3 [II,II,XX]). (* I, I, X *)
+Compute (h_eval GHZ3 [II,ZZ,ZZ]). (* Z, I, Z *)
+Compute (h_eval GHZ3 [XX,ZZ,ZZ]). (* I, I, Z *)
+Compute (h_eval GHZ3 [XX,II,ZZ]). (* Z, Z, Z *)
+Compute (h_eval GHZ3 [XX,ZZ,II]). (* I, Z, I *)
+
+Compute (h_eval GHZ3 [ZZ,II,II]). (* X, X, X *)
+Compute (h_eval GHZ3 [II,ZZ,II]). (* Z, Z, I *)
+Compute (h_eval GHZ3 [II,II,ZZ]). (* I, Z, Z *)
+
+
+Compute (h_eval (CNOT 1 2; CNOT 0 1) [XX*ZZ,XX,XX*ZZ]). (* X, Z, Z ??? *)
+Compute (h_eval (CNOT 0 1; CNOT 1 2) [XX*ZZ,XX,XX*ZZ]). (* X, Z, Z ??? *)
+
+Definition GHZ3' : clifford_ucom 3 :=
+  CNOT 1 2;  
+  CNOT 0 1;
+  H 0.
+
+Compute (h_eval (GHZ3;GHZ3') [ZZ,II,II]). (* Z, I, I *)
+Compute (h_eval (GHZ3;GHZ3') [II,ZZ,II]). (* I, Z, I *)
+Compute (h_eval (GHZ3;GHZ3') [II,II,ZZ]). (* I, I, Z *)
+
+Compute (h_eval (GHZ3;CNOT 1 2) [ZZ,II,II]). (* X, X, I *)
+Compute (h_eval (GHZ3;CNOT 1 2) [II,ZZ,II]). (* Z, Z, I *)
+Compute (h_eval (GHZ3;CNOT 1 2) [II,II,ZZ]). (* I, I, Z *)
+
+
+
+
+
+Compute (
+
 (* Toffoli Decomposition *)
 
 Module TOFF.
