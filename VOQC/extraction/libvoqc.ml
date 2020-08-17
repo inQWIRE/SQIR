00411ff9b810f8741a007c15dfcdde581d337d1a
akshajgaur <akshajgaur@gmail.com>
2020-08-17 13:33:03 -0700

    Added optim functions

--- VOQC/extraction/libvoqc.ml
+++ VOQC/extraction/libvoqc.ml
@@ -1,12 +1,21 @@
 open Optimize
 open Qasm2sqir
 open GateCount
+open GateCancellation
+open NotPropagation
+open HadamardReduction
+open RotationMerging
 
 (* Input/output values are pairs of (circuit * number of qubits).
    The reason we track number of qubits is because it's used by our (naive) write_qasm function.
    Note that none of our current optimizations change the number of qubits used.  *)
 
 let () = Callback.register "optimize" (fun v -> (optimize (fst v), snd v))
+let () = Callback.register "cancel_single_qubit_gates" (fun v -> (cancel_single_qubit_gates (fst v), snd v))
+let () = Callback.register "cancel_two_qubit_gates" (fun v -> (cancel_two_qubit_gates (fst v), snd v))
+let () = Callback.register "hadamard_reduction" (fun v -> (hadamard_reduction (fst v), snd v))
+let () = Callback.register "not_propagation" (fun v -> (not_propagation (fst v), snd v))
+let () = Callback.register "merge_rotations" (fun v -> (merge_rotations (fst v), snd v))
 let () = Callback.register "write_qasm_file" (fun s v -> write_qasm_file s (fst v) (snd v))
 let () = Callback.register "read_qasm_file" get_gate_list 
 let () = Callback.register "x_count" (fun v -> get_x_count (fst v))
