434ec9bee3e5f88b4c62b0c50243e726122d5cc9
akshajgaur <akshajgaur@gmail.com>
2020-08-17 13:32:44 -0700

    Added optim functions and CAML primitives

--- VOQC/extraction/ocaml_wrapper.c
+++ VOQC/extraction/ocaml_wrapper.c
@@ -5,7 +5,6 @@
 #include <caml/memory.h>
 #include <caml/fail.h>
 #include <caml/callback.h>
-
 #include "ocaml_wrapper.h"
 
 // CLOSURE taken from https://github.com/xoolive/facile
@@ -16,10 +15,11 @@
   }
 
 value* wrap(value v) {
+    CAMLparam1(v);
     value* res = (value*) malloc(sizeof(value));
     *res = v;
     caml_register_global_root(res);
-    return res;
+    CAMLreturnT(value*, res);
 }
 
 void destroy(value* v) {
@@ -33,61 +33,117 @@ void init () {
 }
 
 value* read_qasm_file (char* fname) {
-   value local, circ;
+   CAMLparam0();
+   CAMLlocal2(local, circ);
    local = caml_copy_string(fname);
    CLOSURE("read_qasm_file");
    circ = caml_callback(*closure, local);
-   return wrap(circ);
+   CAMLreturnT(value*, wrap(circ));
 }
 
 value* optimize (value* circ) {
-   value res;
+   CAMLparam0();
+   CAMLlocal1(res);
    CLOSURE("optimize");
    res = caml_callback(*closure, *circ);
    destroy(circ);
-   return wrap(res);
+   CAMLreturnT(value*, wrap(res));
+}
+
+value* cancel_single_qubit_gates (value* circ) {
+   CAMLparam0();
+   CAMLlocal1(res);
+   CLOSURE("cancel_single_qubit_gates");
+   res = caml_callback(*closure, *circ);
+   destroy(circ);
+   CAMLreturnT(value*, wrap(res));
+}
+
+value* cancel_two_qubit_gates (value* circ) {
+   CAMLparam0();
+   CAMLlocal1(res);
+   CLOSURE("cancel_two_qubit_gates");
+   res = caml_callback(*closure, *circ);
+   destroy(circ);
+   CAMLreturnT(value*, wrap(res));
+}
+
+value* not_propagation (value* circ) {
+   CAMLparam0();
+   CAMLlocal1(res);
+   CLOSURE("not_propagation");
+   res = caml_callback(*closure, *circ);
+   destroy(circ);
+   CAMLreturnT(value*, wrap(res));
+}
+
+value* hadamard_reduction (value* circ) {
+   CAMLparam0();
+   CAMLlocal1(res);
+   CLOSURE("hadamard_reduction");
+   res = caml_callback(*closure, *circ);
+   destroy(circ);
+   CAMLreturnT(value*, wrap(res));
+}
+
+value* merge_rotations (value* circ) {
+   CAMLparam0();
+   CAMLlocal1(res);
+   CLOSURE("merge_rotations");
+   res = caml_callback(*closure, *circ);
+   destroy(circ);
+   CAMLreturnT(value*, wrap(res));
 }
 
 void write_qasm_file (char* outf, value* circ) {
-   value fname, res;
+  CAMLparam0();
+   CAMLlocal2(fname, res);
    fname = caml_copy_string(outf);
    CLOSURE("write_qasm_file");
    res = caml_callback2(*closure, fname, *circ);
+   CAMLreturn0;
 }
 
 int x_count (value* circ) {
+    CAMLparam0();
     CLOSURE("x_count");
-    return Int_val(caml_callback(*closure, *circ));
+    CAMLreturnT(int, Int_val(caml_callback(*closure, *circ)));
 }
 
 int h_count (value* circ) {
+    CAMLparam0();
     CLOSURE("h_count");
-    return Int_val(caml_callback(*closure, *circ));
+    CAMLreturnT(int, Int_val(caml_callback(*closure, *circ)));
 }
 
 int rz_count (value* circ) {
+    CAMLparam0();
     CLOSURE("rz_count");
-    return Int_val(caml_callback(*closure, *circ));
+    CAMLreturnT(int, Int_val(caml_callback(*closure, *circ)));
 }
 
 int cnot_count (value* circ) {
+    CAMLparam0();
     CLOSURE("cnot_count");
-    return Int_val(caml_callback(*closure, *circ));
+    CAMLreturnT(int, Int_val(caml_callback(*closure, *circ)));
 }
 
 int c_count (value* circ) {
+    CAMLparam0();
     CLOSURE("c_count");
-    return Int_val(caml_callback(*closure, *circ));
+    CAMLreturnT(int, Int_val(caml_callback(*closure, *circ)));
 }
 
 int t_count (value* circ) {
+    CAMLparam0();
     CLOSURE("t_count");
-    return Int_val(caml_callback(*closure, *circ));
+    CAMLreturnT(int, Int_val(caml_callback(*closure, *circ)));
 }
 
 int total_count (value* circ) {
+    CAMLparam0();
     CLOSURE("total_count");
-    return Int_val(caml_callback(*closure, *circ));
+    CAMLreturnT(int, Int_val(caml_callback(*closure, *circ)));
 }
 
 
