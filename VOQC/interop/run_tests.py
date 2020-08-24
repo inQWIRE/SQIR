37740441e5876ed5fa674cb3b253879a79cb6ebf
akshajgaur <akshajgaur@gmail.com>
2020-08-23 20:01:56 -0700

    Added gate decomposition testing

--- VOQC/interop/run_tests.py
+++ VOQC/interop/run_tests.py
@@ -12,3 +12,9 @@ test_cancel_single_qubit_gates()
 test_cancel_two_qubit_gates()
 test_merge_rotations()
 test_hadamard_reduction()
+
+#Test decompositions to VOQC gate set
+test_rzq_decomposition()
+test_u1_decomposition()
+test_u2_decomposition()
+test_u3_decomposition()
--- VOQC/interop/tests/test_qiskit_interop.py
+++ VOQC/interop/tests/test_qiskit_interop.py
@@ -7,10 +7,13 @@ from qiskit.qasm import pi
 from interop.formatting.format_from_qasm import format_from_qasm
 from interop.formatting.rzq_to_rz import rzq_to_rz
 from interop.voqc import SQIR
+from qiskit.transpiler.passes.basis import BasisTranslator
 
 from interop.exceptions import InvalidVOQCFunction
 from qiskit.transpiler import PassManager
 from interop.qiskit.voqc_optimization import VOQC
+from interop.qiskit.voqc_equivalence_library import eq_lib
+from qiskit.converters import circuit_to_dag
 rel = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "benchmarks")
 
 def assert_equality_circuit(before, after, list_opt=None):
@@ -23,21 +26,18 @@ def assert_equality_circuit(before, after, list_opt=None):
 def test_AT():
     before = format_from_qasm(os.path.join(rel, "Arithmetic_and_Toffoli/tof_10.qasm"))
     before = QuantumCircuit.from_qasm_file("copy.qasm")
-    rzq_to_rz(os.path.join((os.path.dirname(os.path.abspath(__file__))), "optimized_qasm_files/optim_tof_10.qasm"))
     after = QuantumCircuit.from_qasm_file(os.path.join((os.path.dirname(os.path.abspath(__file__))), "optimized_qasm_files/optim_tof_10.qasm"))
     assert_equality_circuit(before, after)
     
 def test_PF():
     before = format_from_qasm(os.path.join(rel, "PF/pf2_100.qasm"))
     before = QuantumCircuit.from_qasm_file("copy.qasm")
-    rzq_to_rz(os.path.join((os.path.dirname(os.path.abspath(__file__))), "optimized_qasm_files/optim_pf2_100.qasm"))
     after = QuantumCircuit.from_qasm_file(os.path.join((os.path.dirname(os.path.abspath(__file__))), "optimized_qasm_files/optim_pf2_100.qasm"))
     assert_equality_circuit(before, after)
     
 def test_QFT():
     before = format_from_qasm(os.path.join(rel, "QFT_and_Adders/QFTAdd64.qasm"))
     before = QuantumCircuit.from_qasm_file("copy.qasm")
-    rzq_to_rz(os.path.join((os.path.dirname(os.path.abspath(__file__))), "optimized_qasm_files/optim_QFTAdd64.qasm"))
     after = QuantumCircuit.from_qasm_file(os.path.join((os.path.dirname(os.path.abspath(__file__))), "optimized_qasm_files/optim_QFTAdd64.qasm"))
     assert_equality_circuit(before, after)
     
@@ -86,3 +86,43 @@ def test_hadamard_reduction():
     after.h(0)
     after.sdg(0)
     assert_equality_circuit(before, after, ["hadamard_reduction"])
+
+def test_rzq_decomposition():
+    qasm_str = 'OPENQASM 2.0;\n include "qelib1.inc";\n qreg q[1];\n rz(.375*pi) q[0];'
+    before = QuantumCircuit.from_qasm_str(qasm_str)
+    after = QuantumCircuit(1)
+    after.rz(.375*pi, 0)
+    assert_equality_circuit(before, after)
+    
+def test_u1_decomposition():
+    before = QuantumCircuit(1)
+    before.u1(pi/8, 0)
+    after = QuantumCircuit(1)
+    after.rz(pi/8, 0)
+    dag = (BasisTranslator(eq_lib, ['x', 'h','cx','rz','tdg','sdg','s','t','z'])).run(circuit_to_dag(before))
+    assert (circuit_to_dag(after))==dag
+
+def test_u2_decomposition():
+    before = QuantumCircuit(1)
+    before.u2(pi, pi, 0)
+    after = QuantumCircuit(1)
+    after.rz(3*pi/2, 0)
+    after.h(0)
+    after.rz(pi/2, 0)
+    after.h(0)
+    after.rz(pi/2, 0)
+    dag = (BasisTranslator(eq_lib, ['h','rz'])).run(circuit_to_dag(before))
+    assert (circuit_to_dag(after))==dag
+
+def test_u3_decomposition():
+    before = QuantumCircuit(1)
+    before.u3(3*pi/2, pi/2, pi, 0)
+    after = QuantumCircuit(1)
+    after.rz(pi, 0)
+    after.h(0)
+    after.rz(3*pi/2, 0)
+    after.h(0)
+    after.rz(pi/2, 0)
+    dag = (BasisTranslator(eq_lib, ['x', 'h','cx','rz','tdg','sdg','s','t','z'])).run(circuit_to_dag(before))
+    assert (circuit_to_dag(after))==dag
+
