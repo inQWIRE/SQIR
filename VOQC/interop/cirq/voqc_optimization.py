972daf2a37e8122e9a2429e3cd5325f4057ae413
akshajgaur <akshajgaur@gmail.com>
2020-09-02 19:41:15 -0700

    Added decomposition in qiskit

--- VOQC/interop/cirq/voqc_optimization.py
+++ VOQC/interop/cirq/voqc_optimization.py
@@ -3,11 +3,12 @@ import cirq
 from cirq.contrib.qasm_import import circuit_from_qasm, qasm
 import re
 import os
-
+from cirq import decompose
 from cirq.circuits import Circuit
 from interop.formatting.format_from_qasm import format_from_qasm
 from interop.formatting.rzq_to_rz import rzq_to_rz
 from interop.voqc import SQIR
+from interop.cirq.decompose_cirq_gates import *
 
 class VOQC:
     def __init__(self, func = None):
@@ -19,23 +20,24 @@ class VOQC:
     def optimize_circuit(self, circuit: circuits.Circuit):
         
         #Write qasm file from circuit
+        circuit = Circuit(decompose(circuit, intercepting_decomposer=decompose_library,keep=need_to_keep))
         qasm_str = cirq.qasm(circuit)
         f = open("temp.qasm", "w")
         f.write(qasm_str)
         f.close()
         
         #Decompose gates such as ccz, ccx, u1, u2, u3
-        format_from_qasm("temp.qasm")
+        #format_from_qasm("temp.qasm")
         #Call VOQC optimizations from input list and go from rzq to rz
-        t = self.function_call("copy.qasm")
+        t = self.function_call("temp.qasm")
         rzq_to_rz("temp2.qasm")
         #Get Cirq Circuit from qasm file
         c = open("temp2.qasm").read()
         circ = circuit_from_qasm(c)
     
         #Remove temporary files
-        #os.remove("temp.qasm")
-        #os.remove("copy.qasm")
+        os.remove("temp.qasm")
+        os.remove("temp2.qasm")
         
         return circ
     
@@ -44,5 +46,5 @@ class VOQC:
         for i in range(len(self.func)):
             call = getattr(a,self.func[i])
             call()
-        a.write("temp2.qasm")     
+        return a.write("temp2.qasm")   
     
