4758d811fed0ac001dd03d204c6c769b32ade0eb
Liyi Li <liyili@illinois.edu>
2021-01-09 23:19:56 -0600

    update example QPE/H simulation in QK.v

--- SQIR/src/QK.v
+++ SQIR/src/QK.v
@@ -9,7 +9,7 @@ Local Open Scope nat_scope.
 Definition var := nat.
 
 (* all bound has the form nat * var + nat format. 
-   single var is 1 * var + 0, a constant is 0 * var + nat. *)
+   single var is 1 * var + 0, a constant is 0 * var - nat. *)
 Inductive bound : Set := 
    | Br : nat -> var -> nat -> bound.
 
@@ -18,9 +18,13 @@ Inductive bound : Set :=
 Inductive pred : Set :=
    | Unit : var -> bound -> pred (* x = m * var + m for exactly one *)
    | Lt : var -> bound -> pred (* x < m * var + m *)
+   | Gt : var -> bound -> pred (* x > m * var + m *)
    | Space : var -> nat -> nat -> pred (* forall x, x =  m * x + m for every one that sat the formula. *)
    | Rel : var -> var -> bound -> pred (* forall x, x := m * y + m *)
+   | Pow : var -> bound -> pred (* x = 2 ^ bound *)
    | Fun : var -> (nat -> bool) -> pred (* forall x , f x *)
+   | AnDiv : var -> nat -> bound -> pred (* Angle x = n * Pi / 2 ^ y  *)
+   | Angle : var -> nat -> pred (* Angle x = n *)
    | Conj : pred -> pred -> pred. (* pred /\ pred *)
 
 Definition bound_wf (s : list var) (b:bound) : Prop :=
@@ -29,8 +33,12 @@ Definition bound_wf (s : list var) (b:bound) : Prop :=
 Fixpoint pred_wf (s : list var) (y: option var) (p:pred) : Prop :=
   match p with Unit x br => bound_wf (x::s) br
              | Lt x br => bound_wf (x::s) br
+             | Gt x br => bound_wf (x::s) br
+             | Pow x br => bound_wf s br
              | Space x n m => True
              | Fun x f => True
+             | AnDiv x n br => In x s /\ bound_wf s br
+             | Angle x n => In x s
              | Rel x z br => (match y with None => False | Some y' => (x = z /\ bound_wf (x::s) br) end)
              | Conj p1 p2 => pred_wf s y p1 /\ pred_wf s y p2
   end.
@@ -44,7 +52,7 @@ Inductive fvar : Set := Pair: var -> var -> fvar.
    bcx takes a variable representing the gates, the pred is a predicate defining
    the variable, and variables appears in pred must be bounded by var. *)
 Inductive one_op :=
-  | QX | QY | QH | QZ | QS | QT.
+  | QX | QY | QH | QZ | QS | QT | QHy | QRz.
 
 Inductive two_op := QSWAP | QCX | QCZ.
 
@@ -53,19 +61,20 @@ Inductive bccom : Set :=
 | one (op : one_op) (x:var) (p:pred) (* apply a single qubit gate in a place at x that is defined by predicate p. *)
 | two (op : two_op) (x:var) (y:var) (p:pred)
     (* apply a two qubit gate in a place at x and y that is defined by predicate p. *)
-| cu (x : var) (x:var) (y:var) (p : pred) (bs : list one_op)
+| cu (x:var) (y:var) (p : pred) (b:bccom)
     (* apply a two qubit CU gate in a place at x and y that is defined by predicate p, followinng a series of ops on y. *)
 | bcseq (b1:bccom) (b2:bccom)
    (* seqencing operators *)
-| bciter (b:bccom) (x:var) (p:pred).
+| bciter (x:var) (p:pred)  (b:bccom).
    (* repeat the b x times and x is defined by predicate p. *)
 
 Declare Scope bccom_scope.
 Delimit Scope bccom_scope with bccom.
 Local Open Scope bccom.
 Notation "p1 ; p2" := (bcseq p1 p2) (at level 50) : bccom_scope.
-Parameter x y : var.
+Parameter x y z u v: var.
 Definition const (n:nat) : bound := Br 0 x n.
+Definition variable (a:var) : bound := Br 1 a 0.
 
 (* Example programs using the quantum part of the language. *)
 (* First example is the Simon algorithm. *)
@@ -82,7 +91,24 @@ Definition nCX (n:nat) : bccom := two (QCX) x y (Conj (Lt x (const (n-1))) (Unit
 Definition nX (n:nat) : bccom := one QX x (Lt x (const n)).
 Definition HatN (n:nat) : bccom := one QH x (Unit x (const n)).
 Definition body (n:nat) (U:bccom) := U ; nH n; nX n; HatN (n-1) ; nCX n; HatN (n-1); nX n; nH n.
-Definition grover (i n:nat) (U:bccom) := XatN n; nH (S n); bciter (body (S n) U) x (Unit x (const i)).
+Definition grover (i n:nat) (U:bccom) := XatN n; nH (S n); bciter x (Unit x (const i))  (body (S n) U).
+
+(* Define QPE algorithm. *)
+(* This function defines the controlled_rotations function in QFT.
+   The second function defines QFT.  *)
+Definition cRot (a:var) : bccom := bciter x (Conj (Lt x (variable a)) (Gt x (const 1)))
+                          (cu y z (Conj (Unit y (variable x)) (Unit z (const 0))) (one QRz u (AnDiv u 2 (variable x)))).
+Definition QFT (n:var) : bccom := bciter x (Conj (Lt x (const n)) (Gt x (const 1))) (HatN 0; cRot x); HatN 0.
+Definition cPow (n:nat) (c:bccom) : bccom :=
+    bciter x (Lt x (const n)) (bciter y (Pow y (Br 1 x (1))) (cu u v (Unit u (variable x)) c)).
+Definition QPE (k n:nat) (c:bccom) : bccom := nH (k+n) ;cPow k c; QFT k.
+
+(* Define simple Hamiltonian simulation.
+   See https://github.com/DavitKhach/quantum-algorithms-tutorials/blob/master/Hamiltonian_simulation.ipynb. *)
+Definition HyatN (n:nat) : bccom := one QHy x (Unit x (const n)).
+Definition CXatN (n m:nat) : bccom := two QCX x y (Conj (Unit x (const n)) (Unit y (const m))). 
+Definition H_sim (t:nat) : bccom := HyatN 0; HyatN 1; HatN 2; CXatN 0 1; CXatN 1 2 ;
+              one QRz x (Unit x (const t));  CXatN 1 2 ; CXatN 0 1 ; HyatN 0; HyatN 1; HatN 2.
 
 
 (* Adding load/store opreation to transform qubits to bits. *)
