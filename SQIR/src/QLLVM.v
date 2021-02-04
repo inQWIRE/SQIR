41d8557396b4faeb106706cea6c90ff2e4560f89
Liyi Li <liyili@illinois.edu>
2021-02-03 18:43:13 -0600

    update semantics of QLLVM.

--- SQIR/src/QLLVM.v
+++ SQIR/src/QLLVM.v
@@ -58,7 +58,9 @@ Inductive efun := elet (t:atype) (f:fvar) (ns : list ty_var) (ins : list inst) (
 
 Inductive setting :=setFixedPointNum (n: nat) | setBitLength (n:nat) | efixed (x: list var) | setUncomputeStrategy (x: string).
 
-Inductive top := Prog (ss : list setting) (fs : list efun).
+
+(* a program is with settings, a list of function, and the last fun that is the main fun. *)
+Inductive top := Prog (ss : list setting) (fs : list efun) (main:efun).
 
 
 (* Define the well-formedness of exp. It is SSA + variable-dominance, as well as type match. *)
@@ -107,7 +109,7 @@ Inductive ssa_efuns : list var -> list efun -> Prop :=
    | ssa_efun_many : forall r x xs, ssa_efun r x -> ssa_efuns r xs -> ssa_efuns r (x::xs).
 
 Inductive ssa_top : top -> Prop :=
-  | ssa_thetop : forall ss fs, ssa_efuns [] fs -> ssa_top (Prog ss fs).
+  | ssa_thetop : forall ss fs main, ssa_efuns [] (fs++[main]) -> ssa_top (Prog ss fs main).
 
 
 (* The following relation defines the type system for expressions and instructions and functions. *)
@@ -122,9 +124,14 @@ Module FEnv := FMapList.Make Nat_as_OT.
 
 Definition fenv := FEnv.t (atype * list atype).
 
+Definition type_up_zero (t : atype) : Prop := 
+   match t with (C n) => (0 < n)%nat
+              | (J n) => (0 < n)%nat
+   end.
+
 Inductive type_factor : env -> factor -> atype -> Prop :=
-     | type_jfactor : forall r t x, Env.MapsTo x t r -> type_factor r (Var x) t
-     | type_cfactor_num : forall r n x, type_factor r (Num n x) (C n).
+     | type_jfactor : forall r t x, type_up_zero t -> Env.MapsTo x t r -> type_factor r (Var x) t
+     | type_cfactor_num : forall r n x, (0 < n)%nat -> type_factor r (Num n x) (C n).
 
 
 Definition meet_type (t1 t2: atype) : option atype :=
@@ -154,9 +161,12 @@ Inductive type_exp (fm : fenv) : env -> exp -> atype -> Prop :=
   | ecos_type : forall r x t, type_factor r x t -> type_exp fm r (ecos x) t
   | ecall_type : forall r f xs t ts, FEnv.MapsTo f (t,ts) fm -> type_factors r xs ts 
           -> type_exp fm r (ecall f xs) t
-  | sigma_type : forall r c1 n c2 m f xs t ts, type_factor r c1 (C n) -> type_factor r c2 (C m) ->
-                FEnv.MapsTo f (t,ts) fm -> type_factors r xs ts -> type_exp fm r (sigma c1 c2 f xs) t.
-
+  (*a sigma calls a special non-argument-empty function
+          whose first argument is a C n type and the type matches the type of c1 and c2,
+            as well as any usage of the sigma function has the first position being a variable. *)
+  | sigma_type : forall r c1 c2 n f x xs t ts, type_factor r c1 (C n) -> type_factor r c2 (C n) ->
+                FEnv.MapsTo f (t,((C n)::ts)) fm -> type_factors r ((Var x)::xs) ((C n)::ts)
+                               -> type_exp fm r (sigma c1 c2 f ((Var x)::xs)) t.
 
 Inductive type_inst (fm: fenv) : env -> inst -> env -> Prop :=
    | type_inst_assign : forall r x e t, type_exp fm r e t -> type_inst fm r (assign x e) (Env.add x t r).
@@ -187,9 +197,106 @@ Inductive type_efuns : fenv -> list efun -> fenv -> Prop :=
                     -> type_efuns fm' xs fm'' -> type_efuns fm (x::xs) fm''.
 
 
-Inductive type_top : top -> Prop :=
-  | type_thetop : forall ss fs fm, type_efuns (@FEnv.empty (atype * list atype)) fs fm -> type_top (Prog ss fs).
+(* The current main function is set to have no input argument. Can be changed in the future. *)
+Definition empty_arg (e : efun) : Prop :=
+     match e with elet t f ns ins ret => (ns = []) end.
 
+Inductive type_top : top -> Prop :=
+  | type_thetop : forall ss fs fm main, empty_arg main ->
+        type_efuns (@FEnv.empty (atype * list atype)) (fs++[main]) fm -> type_top (Prog ss fs main).
+
+
+(*The semantics of QLLVM. *)
+Local Open Scope Z_scope.
+
+Module Reg := FMapList.Make Nat_as_OT.
+
+Definition reg := Reg.t (atype * Z).
+
+Definition fmenv := FEnv.t efun.
+
+Definition trunk (t:atype) (n:Z) : Z :=
+   match t with (J 1) | (C 1) => (Z.abs n) mod 2
+              | (C m) | (J m) => n mod (Z_of_nat m)
+   end.
+
+Inductive sem_factor : reg -> factor -> (atype * Z) -> Prop :=
+   | sem_factor_var : forall r x t n, Reg.MapsTo x (t,n) r -> sem_factor r (Var x) (t,n)
+   | sem_factor_num : forall r m n, sem_factor r (Num m n) (C m, trunk (C m) n).
+
+(* The first nat is the number of bits for tenths.
+    There is no way to express sin/cos/sqrt semantics using the real number calculation of them in Coq.
+    They are defined in terms of taylor expansions. *)
+Inductive gen_listval : reg -> list factor -> list Z -> Prop :=
+    | gen_val_empty : forall r, gen_listval r [] []
+    | gen_val_many : forall r xs vs x t v, sem_factor r x (t,v) -> gen_listval r xs vs -> gen_listval r (x::xs) (v::vs).
+
+
+Inductive gen_reg : list Z -> list ty_var -> reg -> Prop :=
+   | gen_reg_empty : gen_reg [] [] (@Reg.empty (atype * Z))
+   | gen_reg_many : forall n ns t x ts r, gen_reg ns ts r -> gen_reg (n::ns) ((t,x)::ts) (Reg.add x (t,n) r).
+
+Definition get_fun_type (f:fvar) (M:fmenv) : option atype :=
+     match (FEnv.find f M) with None => None
+                             | Some (elet t f ns ins ret) => Some t
+     end.
+
+Inductive sem_exp (M: fmenv) (tenth : nat) : reg -> exp -> (atype * Z) -> Prop :=
+   | sem_eplus : forall r x y n1 n2 t1 t2 t3, sem_factor r x (t1,n1) -> sem_factor r y (t2,n2) ->
+                     meet_type t1 t2 = Some t3 -> sem_exp M tenth r (eplus x y) (t3,trunk t3 (n1+n2))
+   | sem_eminus : forall r x y n1 n2 t1 t2 t3, sem_factor r x (t1,n1) -> sem_factor r y (t2,n2) ->
+                     meet_type t1 t2 = Some t3 -> sem_exp M tenth r (eminus x y) (t3,trunk t3 (n1-n2))
+   | sem_emult : forall r x y n1 n2 t1 t2 t3, sem_factor r x (t1,n1) -> sem_factor r y (t2,n2) ->
+                     meet_type t1 t2 = Some t3 -> sem_exp M tenth r (emult x y) (t3,trunk t3 (n1*n2))
+   | sem_ediv : forall r x y n1 n2 t1 t2 t3, sem_factor r x (t1,n1) -> sem_factor r y (t2,n2) ->
+                     meet_type t1 t2 = Some t3 -> sem_exp M tenth r (ediv x y) (t3,trunk t3 (n1 / n2))
+   | sem_emod : forall r x y n1 n2 t1 t2 t3, sem_factor r x  (t1,n1) -> sem_factor r y (t2,n2) ->
+                     meet_type t1 t2 = Some t3 -> sem_exp M tenth r (emod x y) (t3,trunk t3 (n1 mod n2))
+   | sem_efac : forall r x n t, sem_factor r x (t,n)
+                       -> sem_exp M tenth r (efac x) (t,trunk t (Z_of_nat (fact (Z.to_nat (Z.abs n)))))
+   | sem_epow : forall r x y n1 n2 t1 t2, sem_factor r x (t1,n1) -> sem_factor r y (t2,n2) ->
+                                       sem_exp M tenth r (epow x y) (t1, trunk t1 (n1 ^ (Z_of_nat (fact (Z.to_nat (Z.abs n2))))))
+   | sem_ecall : forall r f tu vs ns t v, FEnv.MapsTo f tu M -> gen_listval r vs ns
+                  -> sem_efun M tenth ns tu (t,v) -> sem_exp M tenth r (ecall f vs) (t, v)
+   | sem_sigma : forall r c1 t1 v1 c2 t2 v2 f vs ns tu t v x, sem_factor r c1 (t1,v1) -> sem_factor r c2 (t2,v2) -> v1 < v2
+                   -> get_fun_type f M = Some t -> gen_listval r vs ns -> FEnv.MapsTo f tu M
+                     -> sem_sigma_fun M tenth (Z.to_nat (v2 - v1)) (t,0) (v1::ns) tu (t,v)
+                      -> sem_exp M tenth r (sigma c1 c2 f ((Var x)::vs)) (t,v)
+
+with sem_inst (M: fmenv) (tenth : nat) : reg -> inst -> reg -> Prop :=
+    | sem_theinst : forall r x e, sem_inst M tenth r (assign x e) r
+
+with sem_insts (M: fmenv) (tenth : nat) : reg -> list inst -> reg -> Prop :=
+   | sem_inst_empty : forall r, sem_insts M tenth r [] r
+   | sem_inst_many : forall r x xs, sem_insts M tenth r (x::xs) r
+
+with sem_efun (M: fmenv) (tenth : nat) : list Z -> efun -> (atype * Z) -> Prop :=
+    | sem_thefun : forall r r' vs t t' f ts ins ret v, gen_reg vs ts r ->
+        sem_insts M tenth r ins r' -> Reg.MapsTo ret (t',v) r' ->
+        sem_efun M tenth vs (elet t f ts ins ret) (t,v)
+
+with sem_sigma_fun (M:fmenv) (tenth : nat) : nat -> (atype * Z) -> list Z -> efun -> (atype * Z) -> Prop :=
+    | sem_sigma_fun_empty : forall ns ef tv, sem_sigma_fun M tenth 0 tv ns ef tv
+    | sem_sigma_fun_many : forall n n' ns ef t v v' tv, sem_efun M tenth (n'::ns) ef (t,v')
+                             -> sem_sigma_fun M tenth n (t,v+v') ((n'+1)::ns) ef tv
+                             -> sem_sigma_fun M tenth (S n) (t,v) (n'::ns) ef tv.
+
+
+Fixpoint find_tenth (l : list setting) : option nat :=
+   match l with [] => None
+              | (setFixedPointNum n :: xs) => Some n
+              | (x::xs) => find_tenth xs
+   end.
+
+Fixpoint gen_fmenv (l : list efun) : fmenv :=
+   match l with [] => (@FEnv.empty (efun))
+             | ((elet t f ns ins ret)::xs) => FEnv.add f (elet t f ns ins ret) (gen_fmenv xs)
+   end.
+
+Inductive sem_top : top -> (atype * Z) -> Prop :=
+   | sem_thetop : forall ss fs main n M tv, find_tenth ss = Some n -> gen_fmenv fs = M
+                   -> sem_efun M n [] main tv
+                    -> sem_top (Prog ss fs main) tv.
 
 
 (* The frontend level language. QC. *)
@@ -207,10 +314,3 @@ Inductive cinst := letin (v:var) (e1:QE) (e2:QE).
 
 Inductive ctop := setFixedPointNumC (n: nat) | setBitLengthC (n:nat) 
               | fixedC (x: list var) | setUncomputeStrategyC (x: string) | ins (cs : list cinst).
-
-
-
-
-
-
-
