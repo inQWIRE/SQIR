d3f05aa2252d6b1537d9578c642f605570c24edd
Kesha Hietala <kesha@cs.umd.edu>
2021-09-19 20:51:39 -0400

    filled in most of the proofs in AltPQASM

--- QVM/experiments/AltPQASM.v
+++ QVM/experiments/AltPQASM.v
@@ -33,8 +33,6 @@ Fixpoint gen_srr_gate' (f:vars) (x:var) (n:nat) (size:nat) : ucom U :=
     end.
 Definition gen_srr_gate (f:vars) (x:var) (n:nat) := gen_srr_gate' f x (S n) (S n).
 
-Check control.
-
 Fixpoint controlled_rotations_gen (f : vars) (x:var) (n : nat) (i:nat) : ucom U :=
     match n with
     | 0 | 1 => ID (find_pos f (x,i))
@@ -89,7 +87,7 @@ Fixpoint trans_exp' (f : vars) (dim:nat) (exp:exp) (avs: nat -> posi) : (ucom U 
     end.
 
 (* In the final result, we can throw away the vars and avs. 
-   -> also call eliminate_CU1 to decompose CU1 gates for later optimization *)
+   -> also call decompose_CU1_and_C3X to decompose gates for later optimization *)
 Definition trans_exp f dim exp avs := 
   decompose_CU1_and_C3X (fst (fst (trans_exp' f dim exp avs))).
 
@@ -179,8 +177,6 @@ Definition trans_dmq_cl (size:nat) :=
         | _ => None
    end.
     
-    
-
 (* Also want bc2ucom for comparison's sake *)
 Fixpoint bc2ucom (bc : bccom) : ucom U :=
   match bc with
@@ -190,10 +186,261 @@ Fixpoint bc2ucom (bc : bccom) : ucom U :=
   | bccont a bc1 => control a (bc2ucom bc1)
   | bcseq bc1 bc2 => (bc2ucom bc1) >> (bc2ucom bc2)
   end.
-  
+
+
+(** Proof that these new definitions match the ones in PQASM **)
+
+
+Lemma gen_sr_gate_same : forall dim f x n,
+  to_base_ucom dim (gen_sr_gate f x n) = PQASM.gen_sr_gate f dim x n.
+Proof.
+  intros dim f x n.
+  unfold gen_sr_gate, PQASM.gen_sr_gate.
+  assert (forall a b, 
+          to_base_ucom dim (gen_sr_gate' f x a b) 
+            = PQASM.gen_sr_gate' f dim x a b).
+  { intros. induction a. reflexivity.
+    simpl. rewrite IHa. reflexivity. }
+  apply H.
+Qed.
+
+Lemma gen_srr_gate_same : forall dim f x n,
+  to_base_ucom dim (gen_srr_gate f x n) = PQASM.gen_srr_gate f dim x n.
+Proof.
+  intros dim f x n.
+  unfold gen_srr_gate, PQASM.gen_srr_gate.
+  assert (forall a b, 
+          to_base_ucom dim (gen_srr_gate' f x a b) 
+            = PQASM.gen_srr_gate' f dim x a b).
+  { intros. induction a. reflexivity.
+    simpl. rewrite IHa. reflexivity. }
+  apply H.
+Qed.
+
+Lemma controlled_rotations_gen_same : forall dim f x n i, 
+  to_base_ucom dim (controlled_rotations_gen f x n i)
+    = PQASM.controlled_rotations_gen f dim x n i.
+Proof.
+  intros.
+  destruct n; try reflexivity.
+  induction n; try reflexivity.
+  simpl in *.
+  rewrite IHn.
+  reflexivity.
+Qed.
+
+Lemma controlled_rotations_WF : forall f x n i,
+  well_formed (controlled_rotations_gen f x n i).
+Proof.
+  intros.
+  destruct n.
+  simpl. constructor. auto.
+  induction n.
+  simpl. constructor. auto.
+  remember (S n) as m.
+  simpl.
+  destruct m.
+  lia.
+  constructor.
+  apply IHn.
+  apply control'_WF.
+  constructor. auto.
+Qed.
+
+Lemma QFT_gen_same : forall dim f x n size, 
+  to_base_ucom dim (QFT_gen f x n size) = PQASM.QFT_gen f dim x n size.
+Proof.
+  intros.
+  induction n; try reflexivity.
+  simpl.
+  rewrite IHn.
+  rewrite <- controlled_rotations_gen_same.
+  reflexivity.
+Qed.
+
+Lemma QFT_gen_WF : forall f x n size, well_formed (QFT_gen f x n size).
+Proof.
+  intros.
+  induction n; simpl. 
+  constructor. auto.
+  repeat constructor. 
+  apply IHn.
+  apply controlled_rotations_WF.
+Qed.
+
+Lemma trans_qft_same : forall dim f x,
+  to_base_ucom dim (trans_qft f x) = PQASM.trans_qft f dim x.
+Proof. intros. apply QFT_gen_same. Qed.
+
+Lemma trans_rqft_same : forall dim f x,
+  uc_eval dim (trans_rqft f x) = UnitarySem.uc_eval (PQASM.trans_rqft f dim x).
+Proof. 
+  intros. 
+  unfold trans_rqft, PQASM.trans_rqft.
+  unfold uc_eval.
+  rewrite <- QFT_gen_same.
+  apply invert_same.
+  apply QFT_gen_WF.
+Qed.
+
+Lemma trans_h_same : forall dim f x,
+  to_base_ucom dim (trans_h f x) = PQASM.trans_h f dim x.
+Proof.
+  intros.
+  unfold trans_h, PQASM.trans_h.
+  assert (forall n, to_base_ucom dim (nH f x n) = PQASM.nH f dim x n).
+  { induction n. reflexivity.
+    simpl. rewrite IHn. reflexivity. }
+  apply H.
+Qed.
+
+Lemma trans_exp_same : forall f dim exp avs,
+  uc_eval dim (trans_exp f dim exp avs) 
+    = UnitarySem.uc_eval (fst (fst (PQASM.trans_exp f dim exp avs))).
+Proof.
+  intros.
+  unfold trans_exp.
+  rewrite decompose_CU1_and_C3X_preserves_semantics.
+  assert (forall u1 u2 f1 f2 avs1 avs2,
+          trans_exp' f dim exp avs = (u1, f1, avs1) ->
+          PQASM.trans_exp f dim exp avs = (u2, f2, avs2) ->
+          uc_eval dim u1 =  UnitarySem.uc_eval u2 /\ f1 = f2 /\ avs1 = avs2).
+  { generalize dependent avs.
+    generalize dependent dim.
+    generalize dependent f.
+    induction exp; intros f dim avs u1 u2 f1 f2 avs1 avs2 H1 H2.
+    
+    (* simple cases *)
+    all: try (inversion H1; inversion H2; subst;
+              repeat split; reflexivity). 
+    
+    (* CU p exp *)
+    simpl in H1, H2.
+    destruct (trans_exp' f dim exp avs) eqn:transexp'.
+    destruct p0.
+    destruct (PQASM.trans_exp f dim exp avs) eqn:transexp.
+    destruct p0.
+    eapply IHexp in transexp' as [? [? ?]].
+    2: apply transexp.
+    inversion H1; inversion H2; subst.
+    repeat split; try reflexivity.
+    admit. (* annoying... *)
+
+    (* SR q x *)
+    simpl in H1, H2.
+    unfold uc_eval.
+    inversion H1; inversion H2; subst.
+    rewrite gen_sr_gate_same.
+    auto.
+
+    (* SRR q x *)
+    simpl in H1, H2.
+    unfold uc_eval.
+    inversion H1; inversion H2; subst.
+    rewrite gen_srr_gate_same.
+    auto.
+
+    (* QFT x *)
+    simpl in H1, H2.
+    unfold uc_eval.
+    inversion H1; inversion H2; subst.
+    rewrite trans_qft_same.
+    auto.
+
+    (* RQFT x *)
+    simpl in H1, H2.
+    inversion H1; inversion H2; subst.
+    rewrite trans_rqft_same.
+    auto.
+
+    (* H x *)
+    simpl in H1, H2.
+    unfold uc_eval.
+    inversion H1; inversion H2; subst.
+    rewrite trans_h_same.
+    auto.
+
+    (* exp1 ; exp2 *)
+    simpl in H1, H2.
+    destruct (trans_exp' f dim exp1 avs) eqn:transexp1.
+    destruct p.
+    destruct (PQASM.trans_exp f dim exp1 avs) eqn:transexp2.
+    destruct p.
+    eapply IHexp1 in transexp1 as [? [? ?]].
+    2: apply transexp2.
+    subst.
+    destruct (trans_exp' v0 dim exp2 p1) eqn:transexp3.
+    destruct p.
+    destruct (PQASM.trans_exp v0 dim exp2 p1) eqn:transexp4.
+    destruct p.
+    eapply IHexp2 in transexp3 as [? [? ?]].
+    2: apply transexp4.
+    inversion H1; inversion H2; subst.
+
+    repeat split; try reflexivity.
+    unfold uc_eval in *.
+    simpl.
+    rewrite H, H0.
+    reflexivity. }
+Admitted.
+
+(** Taken from examples/shor/AltShor.v **)
+
 Lemma bc2ucom_WF : forall bc, well_formed (bc2ucom bc).
 Proof.
   induction bc; repeat constructor; auto.
   simpl. unfold control. apply control'_WF.
   assumption.
+Qed.
+
+Lemma bc2ucom_fresh : forall dim q bc,
+  UnitaryOps.is_fresh q (to_base_ucom dim (bc2ucom bc)) <->
+  @UnitaryOps.is_fresh _ dim q (RCIR.bc2ucom bc).
+Proof.
+  intros dim q bc.
+  induction bc; try reflexivity.
+  simpl.
+  destruct bc; try reflexivity.
+  rewrite <- UnitaryOps.fresh_control.
+  unfold control.
+  rewrite <- fresh_control'.
+  rewrite IHbc.
+  reflexivity.
+  lia.
+  apply bc2ucom_WF.
+  rewrite <- UnitaryOps.fresh_control.
+  unfold control.
+  rewrite <- fresh_control'.
+  rewrite IHbc.
+  reflexivity.
+  lia.
+  apply bc2ucom_WF.
+  split; intro H; inversion H; subst; simpl.
+  constructor.
+  apply IHbc1; auto.
+  apply IHbc2; auto.
+  constructor.
+  apply IHbc1; auto.
+  apply IHbc2; auto.
+Qed.
+
+Lemma bc2ucom_correct : forall dim (bc : bccom),
+  uc_eval dim (bc2ucom bc) = UnitarySem.uc_eval (RCIR.bc2ucom bc).
+Proof.
+  intros dim bc.
+  induction bc; try reflexivity.
+  simpl.
+  rewrite control_correct.
+  destruct bc; try reflexivity.
+  apply UnitaryOps.control_ucom_X.
+  apply UnitaryOps.control_cong.
+  apply IHbc.
+  apply bc2ucom_fresh. 
+  apply UnitaryOps.control_cong.
+  apply IHbc.
+  apply bc2ucom_fresh. 
+  apply bc2ucom_WF. 
+  unfold uc_eval in *. simpl.
+  rewrite IHbc1, IHbc2.
+  reflexivity.  
 Qed.
