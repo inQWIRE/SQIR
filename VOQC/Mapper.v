4b1f6c8e1457fd323ab0db0a6ab29c6a3527f87a
lec9243 <lec9243@gmail.com>
2021-12-15 23:43:18 -0500

    65 subgoals

--- VOQC/Mapper.v
+++ VOQC/Mapper.v
@@ -54,6 +54,7 @@ Fixpoint listNN2N (l : list (nat * nat)) : list nat :=
   | (a, b) :: t => a :: b :: (listNN2N t)
   end.
 
+(* Find logical qubits in the first layer that two-qubit gate is applied *)
 Fixpoint first_layer {dim} (l : standard_ucom_l dim) (la : layer): layer :=
   match l with
   | [] => la
@@ -62,26 +63,7 @@ Fixpoint first_layer {dim} (l : standard_ucom_l dim) (la : layer): layer :=
   | _ :: t => first_layer t la
   end.
 
-Fixpoint rest_log_qubits {dim} (l : standard_ucom_l dim) (la : layer) (ls : list nat) : list nat :=
-  match l with
-  | [] => ls
-  | App1 _ n1 :: t =>
-    if (orb (elem_in n1 (listNN2N la)) (elem_in n1 ls))
-    then rest_log_qubits t la ls
-    else rest_log_qubits t la (n1::ls)
-  | App2 _ n1 n2 :: t =>
-    let new_ls := if (orb (elem_in n1 (listNN2N la)) (elem_in n1 ls))
-                  then if (orb (elem_in n2 (listNN2N la)) (elem_in n2 ls))
-                       then ls
-                       else n2 :: ls
-                  else if (orb (elem_in n2 (listNN2N la)) (elem_in n2 ls))
-                      then n1 ::ls
-                      else n1 :: n2 :: ls
-    in
-    rest_log_qubits t la ls
-  | _ :: t => rest_log_qubits t la ls
-  end.
-
+(* Map the logical qubits in the first layer to matching *)
 Fixpoint qmapper dim (m : qmap dim) (mat : matching) (la : layer) : qmap dim :=
   match la with
   | [] => m
@@ -97,96 +79,126 @@ Fixpoint qmapper dim (m : qmap dim) (mat : matching) (la : layer) : qmap dim :=
                         else (snd ( qmapper dim m tmat tla)) q in
         (m1', m2')
       end
-    end.
+  end.
 
 
-Definition initial_qmap {dim} (l : standard_ucom_l dim) (mat : matching) (m : qmap dim): qmap dim :=
+(********************************************
+(* These part is my original design.
+It is tested by the test case written by Kesha. 
+However, it is really hard to prove. Therefore I changed it to the upper version.
+To test it, uncomment this part and comment qmapper function above.  *)
+
+(* Becasue we want all logic qubits can be matched with physical qubits, and
+first_layer function can only find logic qubits that are used by two-qubit gate
+in the first layer, we need this rest_log_qubits to find other qubits
+that are not in the first layer. *)
+Fixpoint rest_log_qubits {dim} (l : standard_ucom_l dim) (la : layer) (ls : list nat) : list nat :=
+  match l with
+  | [] => ls
+  | App1 _ n1 :: t =>
+    if (orb (orb (elem_in n1 (fst_tuple la)) (elem_in n1 (snd_tuple la))) (elem_in n1 ls))
+    then rest_log_qubits t la ls
+    else rest_log_qubits t la (n1::ls)
+  | App2 _ n1 n2 :: t =>
+    let new_ls := if (orb (orb (elem_in n1 (fst_tuple la)) (elem_in n1 (snd_tuple la))) (elem_in n1 ls))
+                 then if (orb (orb (elem_in n2 (fst_tuple la)) (elem_in n2 (snd_tuple la))) (elem_in n2 ls))
+                      then ls
+                      else n2 :: ls
+                 else if (orb (orb (elem_in n2 (fst_tuple la)) (elem_in n2 (snd_tuple la))) (elem_in n2 ls))
+                      then n1 ::ls
+                      else n1 :: n2 :: ls
+    in
+    rest_log_qubits t la ls
+  | _ :: t => rest_log_qubits t la ls
+  end.
+
+Fixpoint lst_N2lst_NN (l : list nat) : layer :=
+  match l with
+  | [] => []
+  | x :: [] => (x,x) :: []
+  |x :: y :: t => (x, y) :: lst_N2lst_NN t
+  end.
+
+(* Original design of qmapper.*)
+Fixpoint qmapper dim (mat : matching) (la : layer) : qmap dim :=
+  match la with
+  | [] =>
+    let m1 q := 0 in
+    let m2 q := 0 in
+    (m1, m2)
+  | [(q1, q2)] =>
+    match (hd (0,0) mat) with
+    | (v1, v2) =>
+      if q1 =? q2 then
+        let m1 q := v1 in
+        let m2 q := q1 in
+        (m1, m2)
+      else
+        let m1 q := if q =? q1 then v1 else v2 in
+        let m2 q := if q =? v1 then q1 else q2 in
+        (m1, m2)
+    end
+  | ((q1, q2) :: t) =>
+    match (qmapper dim (tl mat) t) with
+    | (m1, m2) =>
+      match (hd (0,0) mat) with
+      | (v1, v2) =>
+        let m1' q := if q =? q1 then v1
+                     else if q =? q2 then v2
+                          else m1 q in
+        let m2' q := if q =? v1 then q1
+                     else if q =? v2 then q2
+                          else m2 q in
+        (m1', m2')
+      end
+    end
+  end.
+
+(* This function takes a circuit program, l, and a matching and return the 
+initial mapping function of logic qubits and physiacal qubits. *)
+Definition initial_qmap {dim} (l : standard_ucom_l dim) (mat : matching) : qmap dim :=
   let fst_l := first_layer l [] in
-  let full_layer := fst_l in 
-  qmapper dim m mat full_layer.
+  let full_layer := fst_l ++ (lst_N2lst_NN (rest_log_qubits l fst_l [])) in 
+  qmapper dim mat full_layer.
+
+********end of original code****************)
 
 (****************end of mapper via matching******************)
 
+
+(******* Definition of layout well formed from Kesha *******)
 Definition layout_well_formed dim (m : qmap dim) := 
   forall x, x < dim ->
     log2phys m x < dim /\ 
     phys2log m x < dim /\ 
     phys2log m (log2phys m x) = x /\
     log2phys m (phys2log m x) = x.
+(********* End of WF ***********)
 
-Fixpoint layout_well_formed_b' dim n (m : qmap dim) :=
-  match n with
-  | O => true
-  | S n' => 
-      (log2phys m n' <? dim) &&
-      (phys2log m n' <? dim) &&
-      (phys2log m (log2phys m n') =? n') &&
-      (log2phys m (phys2log m n') =? n') &&
-      (layout_well_formed_b' dim n' m)
-  end. 
-Definition layout_well_formed_b dim (m : qmap dim) :=
-  layout_well_formed_b' dim dim m.
-
-Lemma layout_well_formed_b_equiv : forall dim (m : qmap dim),
-  layout_well_formed_b dim m = true -> layout_well_formed dim m.
-Proof.
-  intros.
-  assert (forall x, layout_well_formed_b' dim x m = true ->
-          forall x0, x0 < x -> log2phys m x0 < dim /\ 
-                         phys2log m x0 < dim /\ 
-                         phys2log m (log2phys m x0) = x0 /\
-                         log2phys m (phys2log m x0) = x0).
-  { intros x Hx x0 Hx0.
-    induction x.
-    lia.
-    simpl in Hx.
-    apply andb_prop in Hx as [Hx Hx5].
-    apply andb_prop in Hx as [Hx Hx4].
-    apply andb_prop in Hx as [Hx Hx3].
-    apply andb_prop in Hx as [Hx1 Hx2].
-    apply Nat.ltb_lt in Hx1.
-    apply Nat.ltb_lt in Hx2.
-    apply beq_nat_true in Hx3.
-    apply beq_nat_true in Hx4.
-    specialize (IHx Hx5).
-    bdestruct (x0 =? x).    
-    subst. 
-    repeat split; assumption.
-    apply IHx.
-    lia. }
-  specialize (H0 dim H).
-  assumption.
-Qed.
-
-Lemma well_formed_log2phys_bij : forall {dim} (m : qmap dim),
-  layout_well_formed dim m ->
-  finite_bijection dim (log2phys m).
-Proof.
-  intros dim m WF.
-  exists (phys2log m).
-  apply WF.
-Qed.  
-
-Lemma well_formed_phys2log_bij : forall {dim} (m : qmap dim),
-  layout_well_formed dim m ->
-  finite_bijection dim (phys2log m).
-Proof.
-  intros dim m WF.
-  exists (log2phys m).
-  intros x Hx.
-  specialize (WF x Hx) as [? [? [? ?]]].
-  auto.
-Qed.
 
 (**************** Proof qmapper well formed ************************)
 Inductive matching_well_typed : nat -> matching -> Prop :=
 | mat_nil dim : matching_well_typed dim nil
-| mat_ind dim : forall p l, (fst p) < dim -> (snd p) < dim -> (fst p) <> (snd p) -> not (In p l) -> matching_well_typed dim l -> matching_well_typed dim (p::l).
+| mat_ind dim : forall p l, (fst p) < dim -> (snd p) < dim ->
+                       (fst p) <> (snd p) ->
+                       not (In (fst p) (listNN2N l)) ->
+                       not (In (snd p) (listNN2N l)) ->
+                       matching_well_typed dim l ->
+                       matching_well_typed dim (p::l).
 
 Inductive layer_well_typed : nat -> layer -> Prop :=
 | la_nil dim : layer_well_typed dim []
-| la_inductive dim : forall n l, (fst n) < dim -> (snd n) < dim -> (fst n) <> (snd n) -> not (In n l) -> layer_well_typed dim l -> layer_well_typed dim (n :: l).
+| la_inductive dim : forall n l, (fst n) < dim -> (snd n) < dim ->
+                            (fst n) <> (snd n) ->
+                            not (In (fst n) (listNN2N l)) ->
+                            not (In (snd n) (listNN2N l)) ->
+                            layer_well_typed dim l ->
+                            layer_well_typed dim (n :: l).
 
+Lemma matching_layer_no_dup : forall (n : nat) (a : nat) (l : list (nat * nat)),
+    not (In n (listNN2N l)) -> In a (listNN2N l) -> n <> a.
+Proof.
+  unfold not. intros. apply H. subst. auto. Qed.
 
 Lemma qmapper_well_formed : forall dim (m : qmap dim) (mat : matching) (la : layer),
     matching_well_typed dim mat ->
@@ -200,8 +212,7 @@ Proof.
   intros H x Hx. 
   destruct (H x Hx) as [? [? [? ?]]].
   induction mat. 
-  - (* first base case *)
-    repeat split.
+  - repeat split.
     do 2 (destruct la; simpl; auto).
     do 2 (destruct la; simpl; auto).
     do 2 (destruct la; simpl; auto).
@@ -210,123 +221,181 @@ Proof.
     simpl. apply H. auto.
     destruct la. simpl.
     destruct IHmat as [? [? [? ?]]].
-    inversion Hmat. auto.
+    inversion Hmat. auto.  
     bdestruct_all.
-    split. subst. rewrite <- H9. auto. 
-    split. subst. auto.
-    split. auto. auto.
-    split. inversion Hmat. auto.
-    split. subst. rewrite H12. auto.
-    split. auto. inversion Hmat. auto. symmetry. admit.
-    split. auto. inversion Hmat. auto.
-    split. inversion Hla. auto.
-    split. auto. auto.
-    split. inversion Hmat. auto.
-    split. rewrite H12. inversion Hla. auto.
-    split. auto. admit.
-    split. inversion Hmat. auto.
-    split. rewrite H13. inversion Hla. auto.
-    split. auto. admit.
-    split. inversion Hmat. auto.
-    split. destruct mat. simpl. auto. simpl. auto.
-    split. auto. destruct mat. simpl. auto. simpl. auto.
-    split. inversion Hmat. auto.
-    split. inversion Hla. auto.
-    split. admit. auto.
-    split. inversion Hmat. auto.
-    split. inversion Hla. auto.
-    split. auto. auto.
-    split. inversion Hmat. auto.
-    split. inversion Hla. auto.
-    split. auto. auto.
-    split. inversion Hmat. auto.
-    split. rewrite H13. inversion Hla. auto.
-    split. inversion Hmat. symmetry in H12. contradiction.
-    inversion Hmat. symmetry in H12. contradiction.
-    split. inversion Hmat. auto.
-    split. rewrite H14. inversion Hla. auto.
-    split. inversion Hmat. symmetry in H12. contradiction.
-    inversion Hmat. symmetry in H12. contradiction.
-    split. inversion Hmat. auto.
-    split. inversion Hmat. symmetry in H12. contradiction.
-    split. inversion Hmat; symmetry in H12; contradiction.
-    inversion Hmat; symmetry in H12; contradiction.
-    split. inversion Hmat. auto.
-    split. rewrite H14. inversion Hla. auto.
-    split. auto. admit.
-    split. inversion Hmat. auto.
-    split. rewrite H15. inversion Hla. auto.
-    split. auto. admit.
-    split.  inversion Hmat. auto.
-    split. destruct mat. simpl. auto. simpl. auto.
-    split. auto. destruct mat. simpl. auto. simpl. auto.
-    split. rewrite H11. inversion Hmat. auto.
-    split. inversion Hla. auto.
-    split. admit. auto.
-    split. rewrite H12. inversion Hmat. auto.
-    split. inversion Hla. auto.
-    split. admit. auto.
-    split. destruct mat. simpl. auto. simpl. auto.
-    split. inversion Hla. auto.
-    split. destruct mat. simpl. auto. simpl. auto. auto.
-    split. destruct mat. simpl. auto. simpl. auto.
-    split. inversion Hla. auto.
-    split. inversion Hla. symmetry in H13. contradiction.
-    inversion Hla. symmetry in H13. contradiction.
-    split. destruct mat. simpl. auto. simpl. auto.
-    split. inversion Hla. auto.
-    split. admit. auto.
-    split. rewrite H13. inversion Hmat. auto.
-    split. inversion Hla. auto.
-    split. admit. admit.
-    split. rewrite H13. inversion Hmat. auto.
-    split. inversion Hla. auto.
-    split. admit. auto.
-    split. destruct mat. simpl. auto. simpl. auto.
-    split. inversion Hla. auto.
-    split. destruct mat. simpl. auto. simpl. auto.
-    admit.
-    split. destruct mat. simpl. auto. simpl. auto.
-    split. inversion Hla. auto.
-    split. destruct mat. simpl. auto. simpl. auto. auto.
-    split. rewrite H12. inversion Hmat. auto.
-    split. rewrite H13. inversion Hla. auto.
-    admit.
-    split. rewrite H12. inversion Hmat. auto.
-    split. rewrite H14. inversion Hla. auto.
-    admit.
-    split. rewrite H12. inversion Hmat. auto.
-    split. destruct mat. simpl. auto. simpl. auto.
-    split. admit.
-    destruct mat. simpl. auto. simpl. auto.
-    split. rewrite H13. inversion Hmat. auto.
-    split. rewrite H14. inversion Hla. auto.
-    admit.
-    split. rewrite H13. inversion Hmat. auto.
-    split. rewrite H15. inversion Hla. auto.
-    admit.
-    split. rewrite H13. inversion Hmat. auto.
-    split. destruct mat. inversion Hmat. auto. inversion Hmat. auto.
-    split. admit.
-    destruct mat. simpl. auto. simpl. auto.
-    destruct mat. repeat split. auto. auto. auto. admit.
-    repeat split. auto. auto. auto. admit.
-    destruct mat. repeat split. auto. auto. auto. admit.
-    repeat split. auto. auto. auto. admit.
-    destruct mat. repeat auto. auto.
-    destruct IHmat as [? [? [? ?]]]. inversion Hmat. auto.
     repeat split.
-    simpl. bdestruct_all. inversion Hmat. auto. inversion Hmat. auto.
-    destruct mat. simpl. auto.
-    simpl. bdestruct_all. inversion Hmat. inversion H18. auto.
-    inversion Hmat. inversion H19. auto. admit.
-    simpl. bdestruct_all. inversion Hla. auto. inversion Hla. auto.
-    destruct mat. simpl. auto.
-    simpl. bdestruct_all. inversion Hla. inversion H18. auto.
-    inversion Hla. inversion H19. auto. admit.
-    simpl. admit.
-    simpl. admit.
-    Admitted.     
-    
+    subst. rewrite <- H9. auto. 
+    subst. auto. auto. auto.
+    repeat split.
+    inversion Hmat. auto.
+    subst. rewrite H12. auto. auto. admit.
+    repeat split.
+    auto. inversion Hmat. auto.
+    inversion Hla. auto. auto. auto.
+    repeat split.
+    inversion Hmat. auto.
+    rewrite H12. inversion Hla. auto. auto. admit.
+    repeat split.
+    inversion Hmat. auto.
+    rewrite H13. inversion Hla. auto. auto. admit.
+    repeat split.
+    inversion Hmat. auto.
+    destruct mat. simpl. auto. simpl. auto.
+    auto. destruct mat. simpl. auto. simpl. auto.
+    repeat split.
+    inversion Hmat. auto. inversion Hla. auto. admit. auto.
+    repeat split.
+    inversion Hmat. auto. inversion Hla. auto. auto. auto.
+    repeat split.
+    inversion Hmat. auto. inversion Hla. auto. auto. auto.
+    repeat split.
+    inversion Hmat. auto.
+    rewrite H13. inversion Hla. auto.
+    inversion Hmat. symmetry in H12. contradiction.
+    inversion Hmat. symmetry in H12. contradiction.
+    repeat split.
+    inversion Hmat. auto.
+    rewrite H14. inversion Hla. auto.
+    inversion Hmat. symmetry in H12. contradiction.
+    inversion Hmat. symmetry in H12. contradiction.
+    repeat split.
+    inversion Hmat. auto.
+    inversion Hmat. symmetry in H12. contradiction.
+    inversion Hmat. symmetry in H12. contradiction.
+    inversion Hmat. symmetry in H12. contradiction.
+    repeat split.
+    inversion Hmat. auto.
+    rewrite H14. inversion Hla. auto. auto. admit.
+    repeat split.
+    inversion Hmat. auto.
+    rewrite H15. inversion Hla. auto. auto. admit.
+    repeat split.
+    inversion Hmat. auto.
+    destruct mat. simpl. auto. simpl. auto.
+    auto. destruct mat. simpl. auto. simpl. auto.
+    repeat split.
+    rewrite H11. inversion Hmat. auto.
+    inversion Hla. auto. admit. auto.
+    repeat split.
+    rewrite H12. inversion Hmat. auto.
+    inversion Hla. auto. admit. auto.
+    repeat split.
+    destruct mat. simpl. auto. simpl. auto.
+    inversion Hla. auto.
+    destruct mat. simpl. auto. simpl. auto. auto.
+    destruct mat. simpl. auto. simpl. auto.
+    repeat split.
+    inversion Hla. auto.
+    inversion Hla. symmetry in H13. contradiction.
+    inversion Hla. symmetry in H13. contradiction. admit.
+    repeat split.
+    destruct mat. simpl. auto. simpl. auto.
+    inversion Hla. auto. admit. admit.
+    repeat split.
+    rewrite H12. inversion Hmat. auto.
+    inversion Hla. auto. admit. auto.
+    repeat split.
+    rewrite H13. inversion Hmat. auto.
+    inversion Hla. auto. admit. admit.
+    repeat split.
+    destruct mat. simpl. auto. simpl. auto.
+    inversion Hla. auto. admit. auto.
+    repeat split.
+    destruct mat. simpl. auto. simpl. auto.
+    inversion Hla. auto. 
+    destruct mat. simpl. auto. simpl. auto. admit.
+    repeat split.
+    destruct mat. simpl. auto. auto.
+    inversion Hla. auto.
+    destruct mat. simpl. auto. simpl. auto. auto.
+    repeat split.
+    rewrite H12. inversion Hmat. auto.
+    rewrite H13. inversion Hla. auto. admit. admit.
+    repeat split.
+    rewrite H12. inversion Hmat. auto.
+    rewrite H14. inversion Hla. auto. admit. admit.
+    repeat split.
+    rewrite H12. inversion Hmat. auto.
+    destruct mat. simpl. auto. simpl. auto. admit.
+    destruct mat. simpl. auto. simpl. auto.
+    repeat split.
+    rewrite H13. inversion Hmat. auto.
+    rewrite H14. inversion Hla. auto. admit. admit. 
+    repeat split.
+    rewrite H13. inversion Hmat. auto.
+    rewrite H15. inversion Hla. auto. admit. admit.
+    repeat split.
+    rewrite H13. inversion Hmat. auto.
+    destruct mat. inversion Hmat. auto.
+    inversion Hmat. auto. admit.
+    destruct mat. simpl. auto. simpl. auto.
+    repeat split.
+    destruct mat; auto; auto.
+    destruct mat. auto. auto.
+    destruct mat. auto. auto. admit.
+    repeat split.
+    destruct mat. auto. auto.
+    destruct mat. auto. auto.
+    destruct mat. auto. auto. admit.
+    repeat split.
+    destruct mat. auto. auto.
+    destruct mat. auto. auto.
+    destruct mat. auto. auto.
+    destruct mat. auto. auto. 
+    destruct IHmat as [? [? [? ?]]].
+    inversion Hmat. auto.
+    simpl. inversion Hmat. inversion Hla.
+    bdestruct_all.
+    repeat split.
+    auto. auto. auto. auto.
+    repeat split.
+    auto. auto. auto. auto.
+    repeat split.
+    auto. rewrite H30. auto. auto. admit.
+    repeat split.
+    auto. rewrite H31. auto. auto. admit.
+    repeat split.
+    auto. admit. auto. admit.
+    repeat split.
+    auto. auto. auto. auto.
+    repeat split.
+    auto. auto. auto. auto.
+    repeat split.
+    auto. rewrite H32. auto. auto. admit.
+    repeat split.
+    auto. rewrite H33. auto. auto. admit.
+    repeat split.
+    auto. admit. auto. admit.
+    repeat split.
+    rewrite H29. auto. auto. admit. auto.
+    repeat split.
+    rewrite H30. auto. auto. admit. auto.
+    repeat split.
+    admit. auto. admit. auto.
+    repeat split.
+    rewrite H30. auto. auto. admit. auto.
+    repeat split.
+    subst. rewrite H31. auto. auto. admit. auto.
+    repeat split.
+    admit. auto. admit. auto.
+    repeat split.
+    rewrite H30. auto. rewrite H31. auto. admit. admit.
+    repeat split.
+    rewrite H30. auto. rewrite H32. auto. admit. admit.
+    repeat split.
+    rewrite H30. auto. admit. admit. admit.
+    repeat split.
+    rewrite H31. auto. rewrite H32. auto. admit. admit.
+    repeat split.
+    rewrite H31. auto. rewrite H33. auto. admit. admit.
+    repeat split.
+    rewrite H31. auto. admit. admit. admit.
+    repeat split.
+    admit. rewrite H32. auto. admit. admit.
+    repeat split.
+    admit. rewrite H33. auto. admit. admit.
+    admit.
 
+Admitted.     
+    
 (**************** Proof End  ************************)
